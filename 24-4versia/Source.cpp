35acf0e0e8a5683c00566cfc06b9879d1fac669b
serg201510 <serg201510@yandex.ru>
2021-06-22 15:45:19 +0300

    Добавьте файлы проекта.

---
+++ 24-4versia/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ifstream file("C:\\24_demo.txt");
+	string buf;
+	int count = 1;
+	int m=1;
+	if (!file)
+	{
+		cout << "ERROR";
+	}
+	file >> buf;
+	for (int i = 1; i <= (buf.size() - 1); i++)
+	{
+		if ((buf[i] == 'X') && (count % 3 == 0) or (buf[i-1] == 'Y') && (count % 3 == 1) or (buf[i] == 'Z') && (count % 3 == 2))
+		{
+			count++;
+			m = max(count, m);
+		}
+		else count = 1;
+	}
+	count << m;
+}
